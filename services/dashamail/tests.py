bf4149b40841d221893439ee92089f68a17fa334
alimandev <aliman@ptolemay.com>
2023-08-29 11:27:23 +0500

    added test for dashamail send

--- services/dashamail/tests.py
+++ services/dashamail/tests.py
@@ -1,26 +1,33 @@
 import unittest
 from unittest.mock import patch
 
-from services.dashamail.send_mail import DashaMailTransaction
+from services.dashamail.send_mail import DashaMailTransaction, DashaMailTransactionException
 
 
 class TestDashaMailTransaction(unittest.TestCase):
 
-    def test_send(self):
-        fake_result = {
-            'http_status_code': 200,
-            'error_code': -1000,
-            'error_message': 'Error Connection'
+    def test_send_success(self):
+
+        emails = ['alimandeveloper@gmail.com']
+        mailing_id = 'mailing_id_123'
+        fake_result_success = {
+            'transaction_id': 'transaction_1',
         }
 
         with patch('services.dashamail.send_mail.DashaMailTransaction.send') as mock_get:
-            mock_get.return_value.http_status_code = 200
-            mock_get.return_value.__dict__ = fake_result
-
-            emails = ['alimandeveloper@gmail.com']
-            mailing_id = 'mailing_id_123'
+            mock_get.return_value = fake_result_success
             dasha_mail = DashaMailTransaction(emails=emails, mailing_id=mailing_id)
             response = dasha_mail.send()
 
-        self.assertEquals(response.http_status_code, 200)
-        self.assertEquals(response.__dict__, fake_result)
+        self.assertEquals(response, fake_result_success)
+
+    def test_send_raise(self):
+
+        emails = ['alimandeveloper@gmail.com']
+        mailing_id = 'mailing_id_123'
+
+        with patch('services.dashamail.send_mail.DashaMailTransaction.send') as mock_get:
+            mock_get.return_value = DashaMailTransaction
+            dasha_mail = DashaMailTransaction(emails=emails, mailing_id=mailing_id)
+
+        self.assertRaises(DashaMailTransactionException, dasha_mail.send)
