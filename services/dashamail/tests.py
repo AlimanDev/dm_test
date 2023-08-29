170fce6625f0e4b35a2e2e7a6e01f42fee8b0456
alimandev <aliman@ptolemay.com>
2023-08-29 11:32:37 +0500

    added test for dashamail send

--- services/dashamail/tests.py
+++ services/dashamail/tests.py
@@ -6,28 +6,26 @@ from services.dashamail.send_mail import DashaMailTransaction, DashaMailTransact
 
 class TestDashaMailTransaction(unittest.TestCase):
 
+    def setUp(self) -> None:
+        super(TestDashaMailTransaction, self).setUp()
+        self.emails = ['alimandeveloper@gmail.com']
+        self.mailing_id = 'mailing_id_123'
+
     def test_send_success(self):
 
-        emails = ['alimandeveloper@gmail.com']
-        mailing_id = 'mailing_id_123'
-        fake_result_success = {
-            'transaction_id': 'transaction_1',
-        }
+        fake_result_success = {'transaction_id': 'transaction_1'}
 
         with patch('services.dashamail.send_mail.DashaMailTransaction.send') as mock_get:
             mock_get.return_value = fake_result_success
-            dasha_mail = DashaMailTransaction(emails=emails, mailing_id=mailing_id)
+            dasha_mail = DashaMailTransaction(emails=self.emails, mailing_id=self.mailing_id)
             response = dasha_mail.send()
 
         self.assertEquals(response, fake_result_success)
 
     def test_send_raise(self):
 
-        emails = ['alimandeveloper@gmail.com']
-        mailing_id = 'mailing_id_123'
-
         with patch('services.dashamail.send_mail.DashaMailTransaction.send') as mock_get:
             mock_get.return_value = DashaMailTransaction
-            dasha_mail = DashaMailTransaction(emails=emails, mailing_id=mailing_id)
+            dasha_mail = DashaMailTransaction(emails=self.emails, mailing_id=self.mailing_id)
 
         self.assertRaises(DashaMailTransactionException, dasha_mail.send)
